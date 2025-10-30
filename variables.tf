1d7d6e66782519ee94b0130cc60c7879763ead49
Samriddha11 <155475136+Samriddha11@users.noreply.github.com>
2025-10-30 10:15:33 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,9 @@
+variable "region" {
+  description = "AWS region for the S3 bucket"
+  type        = string
+  default     = "us-east-1"
+}
+
 variable "bucket_name" {
   description = "Name of the S3 bucket"
   type        = string
