abcdf851deba1f6ac7d4873828e9c5966ad8369b
Samriddha11 <155475136+Samriddha11@users.noreply.github.com>
2025-10-30 10:12:28 +0000

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,46 @@
+variable "bucket_name" {
+  description = "Name of the S3 bucket"
+  type        = string
+}
+
+variable "acl" {
+  description = "ACL for the bucket"
+  type        = string
+  default     = "private"
+}
+
+variable "enable_versioning" {
+  description = "Enable S3 bucket versioning"
+  type        = bool
+  default     = true
+}
+
+variable "enable_sse" {
+  description = "Enable server-side encryption for the bucket"
+  type        = bool
+  default     = true
+}
+
+variable "sse_algorithm" {
+  description = "SSE algorithm. Use 'AES256' for SSE-S3 or 'aws:kms' for SSE-KMS"
+  type        = string
+  default     = "AES256"
+}
+
+variable "kms_key_id" {
+  description = "KMS Key ID or ARN to use when sse_algorithm == 'aws:kms'"
+  type        = string
+  default     = ""
+}
+
+variable "force_destroy" {
+  description = "If true, bucket will be destroyed even if it has objects"
+  type        = bool
+  default     = false
+}
+
+variable "tags" {
+  description = "Additional tags to apply to the bucket"
+  type        = map(string)
+  default     = {}
+}
